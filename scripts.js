a556c1e5efd13c51d44e27c85942c9453e10e8fc
Seth Choate <schoate1@asu.edu>
2024-02-23 15:06:07 -0700

    2/23/2024
    Large push roughly 85% finished

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// code for hamburger menu
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu")
 
@@ -7,3 +8,33 @@ function hamburgerMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+// End of code for ham burger menu
+
+//code for slider
+let slideIndex = 1;
+    showSlides(slideIndex);
+
+//controls for buttons
+function plusSlides(curSlide) {
+    showSlides(slideIndex += curSlide);
+}
+//showing current slide
+function currentSlide(curSlide) {
+    showSlides(slideIndex = curSlide)
+}
+
+function showSlides(curSlide) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+     if (curSlide > slides.length) {
+        slideIndex = 1;
+     }
+     if(curSlide < 1) {
+        slideIndex = slides.length;
+     }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display="none";
+    }
+    slides[slideIndex-1].style.display="block";
+}
+//End of code for slider
